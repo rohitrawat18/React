2a41bbe261a7d9a2f2196e18395b27a034103914
rohitrawat18 <rohit.rawat41161@gmail.com>
2026-08-03 17:43:01 +0530

    update project

---
+++ counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  const [counter, setounter] = useState(15)
+
+  //let counter = 15
+  const addValue = () =>{
+    console.log("clicked", counter);
+    
+    //counter = counter + 1
+
+    setounter(counter + 1)
+  }
+
+  const removeValue = () => {
+
+    setounter(counter - 1)
+  }
+
+
+  return (
+    <>
+      <h1>Chai or React</h1>
+      <h2>Counter value: {counter}</h2>
+      <button 
+      onClick={addValue}
+      >Add Value{counter}</button>
+      <br />
+      <button
+      onClick={removeValue}>remove value {counter} </button>
+      <p>footer: {counter} </p>
+    </>
+  )
+}
+
+export default App
